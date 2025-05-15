79332d4089d6af47b28a58807bf6322ad8d6eea2
aml-709 <topid538@gmail.com>
2025-05-15 12:30:47 +0500

    Initial commit

--- Learn/Learn/settings.py
+++ Learn/Learn/settings.py
@@ -82,16 +82,14 @@ WSGI_APPLICATION = 'Learn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'learnschool',
-        'USER': 'postgres',
-        'PASSWORD': '4679',
-        'HOST': 'localhost',
-        'PORT': '5432',
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
