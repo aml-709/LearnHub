c1bd8118434d50f55b55e6af1df2b0bacde96179
aml-709 <topid538@gmail.com>
2025-05-15 13:38:26 +0500

    Initial commit

--- Learn/Learn/settings.py
+++ Learn/Learn/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,16 +84,10 @@ WSGI_APPLICATION = 'Learn.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'learnshool',
-        'USER': 'postgres',
-        'PASSWORD': '4679',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
